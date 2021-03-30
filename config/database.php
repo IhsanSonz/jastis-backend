51e8c66443fac207e17afcda48bb258672bddad0
IhsanKirson <mnurihsamutaqin66.siswa@smkn4bdg.sch.id>
2021-03-30 18:46:01 +0700

    Connecting to the hosted database

--- config/database.php
+++ config/database.php
@@ -36,14 +36,20 @@ return [
     'connections' => [
 
         'mongodb' => [
-            'driver'   => 'mongodb',
-            'host'     => env('MONGO_DB_HOST', 'localhost'),
-            'port'     => env('MONGO_DB_PORT', 27017),
-            'database' => env('MONGO_DB_DATABASE'),
-            'username' => env('MONGO_DB_USERNAME'),
-            'password' => env('MONGO_DB_PASSWORD'),
-            'options'  => []
+            'driver' => 'mongodb',
+            'dsn' => env('MONGO_DB_DSN'),
+            'database' => env('MONGO_DB_DATABASE', 'homestead'),
         ],
+
+        // 'mongodb' => [
+        //     'driver'   => 'mongodb',
+        //     'host'     => env('MONGO_DB_HOST', 'localhost'),
+        //     'port'     => env('MONGO_DB_PORT', 27017),
+        //     'database' => env('MONGO_DB_DATABASE'),
+        //     'username' => env('MONGO_DB_USERNAME'),
+        //     'password' => env('MONGO_DB_PASSWORD'),
+        //     'options'  => []
+        // ],
 
         'sqlite' => [
             'driver' => 'sqlite',
